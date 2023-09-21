0ffb1308325e3cfe4ad34eadec0e7747d810dcc2
Rita0156 <ritachaudhari006@gmail.com>
2023-09-21 21:31:51 +0530

    cyp

--- buttoncy/src/App.js
+++ buttoncy/src/App.js
@@ -4,7 +4,7 @@ import React from "react"
 function App() {
   const [count,setCount]=React.useState(0)
   return (
-    <div className="App">
+    <div  className="App">
           <h1>The Count is : {count}</h1>
           <button onClick={()=>{setCount(count+1)}}>Add</button>
           <button onClick={()=>{setCount(count-1)}}>Reduce</button>
